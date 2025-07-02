e4ca49e59869b584c43ae75c86db3c112282ab73
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-07-02 15:27:15 +0200

    style card quiz

--- src/app/components/main/card/Card.tsx
+++ src/app/components/main/card/Card.tsx
@@ -1,12 +1,17 @@
-import Link from "next/link"
-import Header from "../../header/Header"
-export default function Card(){
-    return(
-      <div>
-<h3>harry potter</h3>
-<Link href="/quiz">
-<button>start</button></Link>
-      </div>
+import Link from "next/link";
+import Header from "../../header/Header";
+import styles from "./card.module.css";
 
-    )
+export default function Card() {
+  return (
+    <div className={styles.cardContainer}>
+      {/* <Header /> */}
+      <div className={styles.card}>
+        <h3 className={styles.title}>Harry Potter</h3>
+        <Link href="/quiz">
+          <button className={styles.button}>Start</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
