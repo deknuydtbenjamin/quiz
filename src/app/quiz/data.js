76debf4a4c576741d7143406844caee3fc920e3f
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-07-08 07:46:39 +0200

    add new questions

--- src/app/quiz/data.js
+++ src/app/quiz/data.js
@@ -61,5 +61,67 @@ export const quiz ={
     answers: ["1", "2", "3", "4"],
     correct: "3",
   }
-    ]
+    ],
+    questions: [
+  {
+    id: 1,
+    question: "Qui est chargé de porter l'Anneau Unique ?",
+    answers: ["Frodon", "Gandalf", "Aragorn", "Legolas"],
+    correct: "Frodon"
+  },
+  {
+    id: 2,
+    question: "Quel est le nom du magicien blanc ?",
+    answers: [ "Gandalf", "Radagast", "Alatar", "Saroumane"],
+    correct: "Saroumane"
+  },
+  {
+    id: 3,
+    question: "Quel est le royaume d’Aragorn ?",
+    answers: [ "Rohan", "Mordor", "Gondor","Eriador"],
+    correct: "Gondor"
+  },
+  {
+    id: 4,
+    question: "Comment s'appelle l'épée reforgée d'Aragorn ?",
+    answers: [ "Narsil", "Andúril", "Sting", "Glamdring"],
+    correct: "Andúril"
+  },
+  {
+    id: 5,
+    question: "Quel est le nom de la ville souterraine des nains ?",
+    answers: ["Moria", "Gondolin", "Dale", "Erebor"],
+    correct: "Moria"
+  },
+  {
+    id: 6,
+    question: "Comment s'appelle le jardinier et ami de Frodon ?",
+    answers: [ "Pippin", "Merry", "Bilbon", "Sam"],
+    correct: "Sam"
+  },
+  {
+    id: 7,
+    question: "Quel est le nom de la montagne où l'Anneau doit être détruit ?",
+    answers: [ "Mont Solitaire", "Mont Brumeux", "Mont Destin", "Mont du Feu"],
+    correct: "Mont Destin"
+  },
+  {
+    id: 8,
+    question: "Qui est le Seigneur des Ténèbres ?",
+    answers: ["Sauron", "Saroumane", "Morgoth", "Nazgûl"],
+    correct: "Sauron"
+  },
+  {
+    id: 9,
+    question: "Quel est le nom du roi du Rohan ?",
+    answers: ["Théoden", "Denethor", "Éomer", "Boromir"],
+    correct: "Théoden"
+  },
+  {
+    id: 10,
+    question: "Quel peuple vit à Fondcombe ?",
+    answers: ["Les Elfes", "Les Nains", "Les Hommes", "Les Hobbits"],
+    correct: "Les Elfes"
+  }
+]
 }
